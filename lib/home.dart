f502007a3ed7d8e3e102d7b784d399c2378f5142
cerveau08 <malickcoly342@gmail.com>
2020-08-28 16:45:09 +0000

    drawer ok

--- lib/home.dart
+++ lib/home.dart
@@ -19,6 +19,105 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Builder(
+            builder: (context) => IconButton(
+              icon: SvgPicture.asset("assets/icons/drawer.svg"),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+            )
+          ),
+        ),
+        titleSpacing: 0,
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            SizedBox(height: 30,),
+            DrawerHeader(
+              child: Container(
+                height: 140,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset("assets/images/ten_news.png"),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Text(
+              "Profile",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600
+              ),
+            ),
+            SizedBox(height: 25,),
+            Text(
+              "Settings",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600
+              ),
+            ),
+            SizedBox(height: 25,),
+            Text(
+              "About",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600
+              ),
+            ),
+            SizedBox(height: 25,),
+            Text(
+              "Log Out",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600
+              ),
+            ),
+            SizedBox(height: 25,),
+            Material(
+              borderRadius: BorderRadius.circular(500),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(500),
+                splashColor: Colors.black45,
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Colors.black,
+                  child: Icon(
+                    Icons.arrow_back, 
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 45,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.black,
+                  child: Center(
+                    child: Text(
+                      'v1.0.1',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       body: <Widget>[
         Container(color: Colors.blue,),
         Container(color: Colors.red,),
